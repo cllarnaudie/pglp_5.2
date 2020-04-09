76a259599611e37a335109d64a9db1e698bef008
Larnaudie <claire.larnaudie@ens.uvsq.fr>
2020-04-09 22:31:35 +0200

    corrections dans les classes

---
+++ PersonnelJdbc/src/main/java/Main.java
@@ -0,0 +1,62 @@
+package main.java;
+
+
+
+import java.time.Month;
+
+public class Main {
+	
+	private static Main INSTANCE; 
+	
+	private Main() {
+		
+	}
+	
+	public static Main getInstance () {
+		if (INSTANCE == null) {
+			INSTANCE = new Main(); 
+		}
+		return INSTANCE; 
+	}
+
+     public static void main (String [] args) {
+    	 
+    	 GroupePersonnel gp = new GroupePersonnel ();  	
+ 		Personnel p1 = new Personnel.Builder("Gertrude", "Germaine")
+ 				       .build(); 
+ 	
+		Personnel p2 = new Personnel.Builder("Jean", "Bouysous")
+				        .build(); 
+		
+		Personnel p3 = new Personnel.Builder("Marcel ", "Pagnol ")
+			       .build();
+
+		
+	    System.out.println(p3.getPrenom() + " " + p3.getNom() + " " + p3.getDateNaissance() +
+					" "+ p3.getFonction()+ " " + p3.getNumeroTelephone() + "\n");
+	
+ 		
+ 		gp.ajouter_personnel(p1);
+ 		gp.ajouter_personnel(p2);
+ 		gp.ajouter_personnel(p3);
+ 		
+ 		gp.supprimer_personnel(p1);
+ 		gp.supprimer_personnel(p2);
+ 		
+ 		
+
+	    Personnel perso = new Personnel.Builder ("Desmoulins", "Camille")	    		
+	    		      .fonction("journaliste")
+	    		      .dateNaissance (1980,Month.MAY,24)
+	    		      .numeroTelephone("0130859674")
+	    		      .numeroTelephone("0160325487")
+	    	          .build() ; 
+	    
+	    System.out.println(perso.getPrenom() + " " + perso.getNom() + " " + perso.getDateNaissance() +
+				" "+ perso.getFonction()+ " " + perso.getNumeroTelephone() );
+		System.out.println("\n");	
+ 	
+     }
+    	 
+}
+
