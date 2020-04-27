cecaf7e5d88c225d80bfb4b449875a6b63f8863d
Larnaudie <claire.larnaudie@ens.uvsq.fr>
2020-04-27 12:12:21 +0200

    ajout classe jdbc  test

---
+++ PersonnelJdbc/src/test/java/jdbc/TestJdbcConnexionDerby.java
@@ -0,0 +1,22 @@
+package test.java.jdbc;
+
+import java.sql.Connection;
+import org.junit.Test;
+import main.java.dao.jdbc.JdbcConnexionDerby;
+
+public class TestJdbcConnexionDerby {
+
+	@Test
+	public void testConnexion() {
+		
+		JdbcConnexionDerby driver = new JdbcConnexionDerby("usersdb", "/home/devc/tools/derby/");
+	      
+		/**connexion a la base*/
+		Connection connect = driver.getChaineConnexion();
+		
+		/**fermeture de la connexion*/
+		JdbcConnexionDerby.closeConnexion(connect);
+		
+	}
+
+}
